43492bf300eaf955d39afab2a12eab7cd210fb3d
michal55 <michal.kren94@gmail.com>
2017-03-22 11:19:54 +0100

    Extract shape frm comment

--- import.py
+++ import.py
@@ -14,7 +14,7 @@ pd.set_option('display.width', 500)
 
 plt.rcParams.update({'font.size': 14})
 
-df = pd.read_csv('scrubbed.csv',  dtype='unicode')
+df = pd.read_csv('/home/michal/workspace/OZ/scrubbed.csv',  dtype='unicode')
 print(df.columns)
 # df.query('state==tx')
 
@@ -22,21 +22,63 @@ print(df.columns)
 
 # print(df.groupby('state').value_counts())
 # print(df['shape'].value_counts())
-# df = 
-plt.figure()
-# df = df['shape'].value_counts()
-# print(df)
-# df[df > 50].plot.bar()
 
+def shape_from_comment( row ):
+	comment = row['comments']
+	shapes = ['cylinder', 'light', 'circle', 'sphere', 'disk', 'fireball', 'unknown', 'oval',
+	 'other', 'cigar', 'rectangle', 'chevron', 'triangle', 'formation', 'delta',
+	 'changing', 'egg', 'diamond', 'flash', 'teardrop', 'cone', 'cross', 'pyramid',
+	 'round', 'crescent', 'flare', 'hexagon', 'dome', 'changed']
+	if isinstance(comment, str):
+		result = (' ').join(set(shapes).intersection(set(comment)))
+		if len(result) > 1:
+			return result
+		else:
+			print(comment)
+			return np.nan
+	else:
+		return "None"
+
+def first_word( row ):
+	if isinstance(row['comments'], str):
+		return (row['comments'].partition(' ')[0])
+	else:
+		return "None"
+
+plt.figure()
+shapes = df['shape'].unique()
+print(shapes)
+
+# comments = df['comments']
+# print(comments.size)
+# print(len(df))
+
+emtpy_shapes = df.query("(shape != shape)")
+print(emtpy_shapes.size)
+
+emtpy_shapes['shape'] = emtpy_shapes.apply(shape_from_comment, axis=1)
+print(emtpy_shapes)
+# print(emtpy_shapes)
+
+print(df.query("(shape != shape)").size)
+
+
+# first_word(emtpy_shapes['comments'].iloc[1])
+# shape_from_comment(df['comments'][0])
+# emtpy_shapes['shape'] = emtpy_shapes['']
+
+
+
+# df[df > 50].plot.bar()
 # df = df['duration (seconds)']
 
 
-dc = df['duration (seconds)'].value_counts().to_dict()
+# dc = df['duration (seconds)'].value_counts().to_dict()
 # df = df.query("(duration (seconds) < 60)")
 # .value_counts()
 
-d = dict((k, v) for k, v in dc.items() if k <= '60')
-print(d)
+# d = dict((k, v) for k, v in dc.items() if k <= '60')
+# print(d)
 
 
 # df = df['duration (seconds)'].value_counts().hist(bins=list(range(0,60,1))).figure
