bb2c2ddca757918b154546469765b215586e7ead
f4rnham <f4rnham@users.noreply.github.com>
2017-05-16 21:04:35 +0200

    Evaluation

--- src/dbscan_simple.py
+++ src/dbscan_simple.py
@@ -1,26 +1,104 @@
 import random
+import os
+import pickle
 import time
+import sys
 import concurrent.futures
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.spatial import distance
 
+import jqmcvi.base as jqmcvi
 from preprocessing import preprocess_ufo_data
+
+def similarity(array, output):
+    sim = []
+    for X in range(len(array)):
+        sim.append([])
+        for Y in range(len(array)):
+            sim[X].append((distance.cosine(array[X], array[Y])+1)**2)
+            # print(sim[X][Y])
+    if output == "arr":
+        return sim
+    else:
+        summed = []
+        for X in range(len(array)):
+            summed.append(sum(sim[X]))
+        return summed           
+
+
+def cluster_sim(centroid, cluster):
+    sim = []
+    for element in cluster:
+        sim.append((distance.cosine(centroid, element)+1)**2)
+    return sum(sim)
 
 
 def DBScan(df, eps, min_samples):
-    # Scaling magic
-    scaler = StandardScaler().fit(df)
-    df = scaler.transform(df)
+    file_name = 'dbscan_' + str(eps) + '_' + str(min_samples)
+
+    if len(sys.argv) >= 3 and sys.argv[2] == 'skip' and not os.path.isfile(file_name):
+        return 0.
 
     # DBSCAN
-    db = DBSCAN(eps = eps, min_samples = min_samples, algorithm='auto').fit(df)
+    db = None
+    if os.path.isfile(file_name):
+        db = pickle.load(open(file_name, 'rb'))
+    else:
+        db = DBSCAN(eps = eps, min_samples = min_samples, algorithm='auto').fit(df)
+        pickle.dump(db, open(file_name, 'wb'))
 
-    # Reverse scaling if we need it for calculations
-    #df = scaler.inverse_transform(df)
+    labels = db.labels_
+    unique_labels = set(labels)
+    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+    n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+
+    print('Clusters for', eps, min_samples, n_clusters)
+
+    if n_clusters == 0:
+        return 0.
+
+    clusters = []
+    centers = []
+    in_cluster_sim = []
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            continue
+
+        class_member_mask = (labels == k)
+
+        cluster = df[class_member_mask]
+        clusters.append(cluster)
+
+        center = np.average(cluster, axis = 0)
+        centers.append(center)
+
+        in_cluster_sim.append(cluster_sim(center, cluster))
+
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == 'db':
+            return jqmcvi.davisbouldin(clusters, centers)
+        elif sys.argv[1] == 'custom':
+            cluster_similarities = similarity(centers, "else")
+
+            sum_cross_cluster = sum(cluster_similarities)
+            avg_cross_sum = sum_cross_cluster / float(len(centers))
+
+            print("Average cross-cluster similarity: ", avg_cross_sum)
+
+            intra_sum = sum(in_cluster_sim)
+            avg_intra_sum = intra_sum/float(len(in_cluster_sim))
+
+            print("Average intra-cluster similarity: ", avg_intra_sum)
+            print("Number of clusters: ", len(centers))
+            print("\nIntra / Cross: ", avg_intra_sum / avg_cross_sum)
+            return min(4.0, avg_intra_sum / avg_cross_sum)
 
     # Evaluate clustering
     return random.random()
@@ -37,13 +115,17 @@ df = pd.concat([df[['latitude', 'longitude', 'timestamp', 'season_x', 'season_y'
  'day_of_year_x', 'day_of_year_y', 'hour_of_day', 'hour_of_day_x',
  'hour_of_day_y' ]], pd.get_dummies(df['shape']), pd.get_dummies(df['season']), pd.get_dummies(df['time_of_day'])], axis=1)
 
-MIN_SAMPLES_START = 1
-MIN_SAMPLES_END = 100
-MIN_SAMPLES_COUNT = 4
+# Scaling magic
+scaler = StandardScaler().fit(df)
+df = scaler.transform(df)
 
-EPS_START = 0.1
-EPS_END = 1
-EPS_COUNT = 4
+MIN_SAMPLES_START = 10
+MIN_SAMPLES_END = 100
+MIN_SAMPLES_COUNT = 10
+
+EPS_START = 0.3
+EPS_END = 3.0
+EPS_COUNT = 10
 
 x_data = np.linspace(MIN_SAMPLES_START, MIN_SAMPLES_END, MIN_SAMPLES_COUNT)
 y_data = np.linspace(EPS_START, EPS_END, EPS_COUNT)
@@ -63,7 +145,10 @@ def solve_for(idx):
     concurrent.futures.wait(arr)
 '''
 
-[solve_for(i) for i in range(0, len(x_data))]
+#[solve_for(i) for i in range(0, len(x_data))]
+
+z_data = pickle.load(open('custom_z', 'rb'))
+#pickle.dump(z_data, open('custom_z', 'wb'))
 
 print('Plotting...')
 
@@ -77,9 +162,10 @@ x_data, y_data = np.meshgrid(x_data, y_data)
 x_data = x_data.flatten()
 y_data = y_data.flatten()
 z_data = z_data.flatten()
-ax.bar3d(x_data, y_data, np.zeros(len(z_data)),
-        (MIN_SAMPLES_END - MIN_SAMPLES_START) / MIN_SAMPLES_COUNT,
-        (EPS_END - EPS_START) / EPS_COUNT, z_data )
+#ax.bar3d(x_data, y_data, np.zeros(len(z_data)),
+#        (MIN_SAMPLES_END - MIN_SAMPLES_START) / MIN_SAMPLES_COUNT - 1.,
+#        (EPS_END - EPS_START) / EPS_COUNT - 0.07, z_data )
+ax.plot_surface(x_data, y_data, z_data, rstride = 1, cstride = 1)
 
 ax.set_xlabel('min samples')
 ax.set_ylabel('eps')
